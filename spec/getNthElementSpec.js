f4d8eb85ca8391ddcb21a259bf026a8fde72e774
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-07 21:38:29 +0530

    Installation of backbone and underscore

---
+++ spec/getNthElementSpec.js
@@ -0,0 +1,13 @@
+
+
+describe("getNthElement", function () {
+
+    it("should be defined", function () {
+        expect(window.getNthElement).toBeDefined();
+    });
+
+    it("should return nth element", function () {
+        const result = getNthElement([1, 2, 3], 0);
+        expect(result).toEqual(1);
+    });
+});
